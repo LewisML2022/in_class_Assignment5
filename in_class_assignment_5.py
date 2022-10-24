c95bbcec3aafdfd2c5af8ab28bf5641e39f9ef95
LewisML2022 <116318284+LewisML2022@users.noreply.github.com>
2022-10-23 22:08:40 -0400

    Add files via upload

--- in_class_assignment_5.py
+++ in_class_assignment_5.py
@@ -23,20 +23,62 @@ The steps of the algorithm are as follows:
 The base cases occur when the sublists are either empty or have one element, as these are inherently sorted. 
  '''
 
+import random
 
-def quicksort(numbers_in_a_list):
+def quicksort(content_list):
 
 #WRITE YOUR CODE HERE FOR THE RECURSIVE SORTING FUNCTION
+# Modified from https://www.koderdojo.com/blog/quicksort-algorithm-in-python
+    def sort(lst, l, r):
+        # base case
+        if r <= l:
+            return
 
-    return #WHAT DOES IT RETURN?
+        # choose random pivot
+        pivot_index = random.randint(l, r)
 
+        # move pivot to first index
+        lst[l], lst[pivot_index] = lst[pivot_index], lst[l]
+
+        # partition
+        i = l
+        for j in range(l+1, r+1):
+            if lst[j] < lst[l]:
+                i += 1
+                lst[i], lst[j] = lst[j], lst[i]
+
+        # place pivot in proper position
+        lst[i], lst[l] = lst[l], lst[i]
+
+        # sort left and right partitions
+        sort(lst, l, i-1)
+        sort(lst, i+1, r)
+
+    if content_list is None or len(content_list) < 2:
+        return
+
+    sort(content_list, 0, len(content_list) - 1)
+    
+    print('\nThe sorted list is: ', content_list)
 
 def main():
 
 # WRITE YOUR MAIN FUNCTION HERE TO READ IN YOUR numbers.txt FILE, RUN THE LIST THROUGH YOUR SORTING ALGORITHM, 
 # AND WRITE OUT YOUR FILE
-
-    return #WHAT DOES IT RETURN?
+    # modified from https://appdividend.com/2021/06/21/how-to-read-file-into-list-in-python/   
+    txt_file = open("numbers.txt", "r")
+    file_content = txt_file.read()
+    print("The file contents are: ", file_content)
+    
+    content_list = file_content.replace('[', '').replace(']','').replace(' ', '').split(",")
+    txt_file.close()
+    content_list = [int(i) for i in content_list]
+    print("\nThe list is: ", content_list)
+    
+    sorted_list = open("sorted.txt", "w")
+    sorted_list.write("\n".join(str(i) for i in content_list))
+    
+    return content_list #WHAT DOES IT RETURN?
 
 
 if __name__ == "__main__":
